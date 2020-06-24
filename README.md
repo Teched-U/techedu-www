e03d04a3f325c44e22d61b12367cf669633a7037
Ricky Xu <poppinricky@fb.com>
2020-06-24 09:51:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Installation
+1.  Install NodeJs from NodeJs Official Page : https://nodejs.org/en/?ref=creativetim
+2.  Go to the root directory of this project from terminal 
+3.  `npm install`
+4.  `npm start`
+
+Check out my cute shinchan video at Watch page
+
---
+++ src/views/Watch/Watch.js
@@ -0,0 +1,92 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+// core components
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardAvatar from "components/Card/CardAvatar.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
+
+import avatar from "assets/img/faces/marc.jpg";
+import xiaoxin from "assets/img/xiaoxin.jpg";
+
+// Import for Video
+import "video-react/dist/video-react.css"; // import css
+import { Player } from 'video-react';
+
+
+const styles = {
+  cardCategoryWhite: {
+    color: "rgba(255,255,255,.62)",
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    marginBottom: "0"
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none"
+  }
+};
+
+const useStyles = makeStyles(styles);
+
+export default function UserProfile() {
+  const classes = useStyles();
+  return (
+    <div>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={8}>
+          <Card>
+            <CardHeader color="primary">
+              <h4 className={classes.cardTitleWhite}>TechedU Class</h4>
+              <p className={classes.cardCategoryWhite}>Be focused!</p>
+            </CardHeader>
+            <CardBody>
+              <Player
+                playsInline
+                poster={xiaoxin}
+                src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+              />
+            </CardBody>
+            <CardFooter>
+              <Button color="primary">Like the Video!</Button>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={4}>
+          <Card profile>
+            <CardAvatar profile>
+              <a href="#pablo" onClick={e => e.preventDefault()}>
+                <img src={avatar} alt="..." />
+              </a>
+            </CardAvatar>
+            <CardBody profile>
+              <h6 className={classes.cardCategory}>CEO / CO-FOUNDER</h6>
+              <h4 className={classes.cardTitle}>Alec Thompson</h4>
+              <p className={classes.description}>
+                Don{"'"}t be scared of the truth because we need to restart the
+                human foundation in truth And I love you like Kanye loves Kanye
+                I love Rick Owens’ bed design but the back is...
+              </p>
+              <Button color="primary" round>
+                Follow
+              </Button>
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
+}
